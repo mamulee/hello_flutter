a4814da459dee66e93e3806186bb4e84b73a3d6a
mamulee <ghdah7@gmail.com>
2023-12-02 02:55:06 +0900

    image & singlechildscrollview

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,31 @@ class MyApp extends StatelessWidget {
             style: TextStyle(fontSize: 28),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: "이메일"),
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(labelText: "비밀번호"),
-              ),
-              Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 24),
-                  child: ElevatedButton(onPressed: () {}, child: Text('로그인')))
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://i.ibb.co/nngK6j3/startup.png",
+                    width: 81,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "이메일"),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "비밀번호"),
+                ),
+                Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(top: 24),
+                    child: ElevatedButton(onPressed: () {}, child: Text('로그인')))
+              ],
+            ),
           ),
         ),
       ),
